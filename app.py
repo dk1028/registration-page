62491b0530adfb0590ea4d86c723eb9aafae2c44
dk1028 <kimdowoo3@gmail.com>
2023-12-30 22:41:40 -0500

    adding files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect #render_template으로 html파일 렌더링
+from flask import Flask, render_template, request, redirect 
 from models import db
 import os
 from models import Fcuser
@@ -7,46 +7,43 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello, user!"
-#GET = 페이지가 나오도록 요청. POST = 버튼을 눌렀을때 데이터를 가지고오는 요청/ 요청정보확인하려면 request 임포트 필요
+
 @app.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'GET':
         return render_template("register.html")
     else:
-        #회원정보 생성
         userid = request.form.get('userid') 
         username = request.form.get('username')
         password = request.form.get('password')
         re_password = request.form.get('re_password')
-        print(password) # 들어오나 확인해볼 수 있다. 
-
+        print(password) 
 
         if not (userid and username and password and re_password) :
-            return "모두 입력해주세요"
+            return "Write all info"
         elif password != re_password:
-            return "비밀번호를 확인해주세요"
-        else: #모두 입력이 정상적으로 되었다면 밑에명령실행(DB에 입력됨)
+            return "Verify your password"
+        else: 
             fcuser = Fcuser()         
-            fcuser.password = password           #models의 FCuser 클래스를 이용해 db에 입력한다.
+            fcuser.password = password           
             fcuser.userid = userid
             fcuser.username = username      
             db.session.add(fcuser)
             db.session.commit()
-            return "회원가입 완료"
+            return "Successfully registered"
 
         return redirect('/')
 
 if __name__ == "__main__":
-    basedir = os.path.abspath(os.path.dirname(__file__))  # database 경로를 절대경로로 설정함
-    dbfile = os.path.join(basedir, 'db.sqlite') # 데이터베이스 이름과 경로
+    basedir = os.path.abspath(os.path.dirname(__file__))  
+    dbfile = os.path.join(basedir, 'db.sqlite')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True     # 사용자에게 원하는 정보를 전달완료했을때가 TEARDOWN, 그 순간마다 COMMIT을 하도록 한다.라는 설정
-    #여러가지 쌓아져있던 동작들을 Commit을 해주어야 데이터베이스에 반영됨. 이러한 단위들은 트렌젝션이라고함.
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   # True하면 warrnig메시지 유발, 
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True     
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   
 
-    db.init_app(app) #초기화 후 db.app에 app으로 명시적으로 넣어줌
+    db.init_app(app) 
     db.app = app
-    db.create_all()   # 이 명령이 있어야 생성됨. DB가
+    db.create_all()  
 
 
     app.run(host='127.0.0.1', port=5000, debug=True) 
--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()           #SQLAlchemy를 사용해 데이터베이스 저장
+db = SQLAlchemy()           
 
 class Fcuser(db.Model): 
-    __tablename__ = 'fcuser'   #테이블 이름 : fcuser
-    id = db.Column(db.Integer, primary_key = True)   #id를 프라이머리키로 설정
-    password = db.Column(db.String(64))     #패스워드를 받아올 문자열길이 
-    userid = db.Column(db.String(32))       #이하 위와 동일
+    __tablename__ = 'fcuser'  
+    id = db.Column(db.Integer, primary_key = True)   
+    password = db.Column(db.String(64))     
+    userid = db.Column(db.String(32))       
     username = db.Column(db.String(8))
